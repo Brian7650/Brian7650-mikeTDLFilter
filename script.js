f63eb9757759a9e3fe22dddf146afd53621309e2
Brian B <brtl567@gmail.com>
2022-04-04 13:20:18 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+// Select DOM
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event Listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+
+// Functions
+function addTodo(event) {
+    // Prevent form from submiiting
+    event.preventDefault();
+
+    // Create todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    // Create List (li)
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    // this put the newTodo inside todoDiv
+
+    // Check Mark Button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    // Check Trash Button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fa-solid fa-circle-minus"></i>`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    // Append To List
+    todoList.appendChild(todoDiv);
+
+    // Clear todoInput value
+    todoInput.value = "";
+}
+
+// Delete Check Function
+function deleteCheck(event) {
+    const item = event.target;
+    // Delete todo
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        // Delete Animation
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend", function() {
+          todo.remove();  
+        }); 
+    }
+    // Check Mark
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
